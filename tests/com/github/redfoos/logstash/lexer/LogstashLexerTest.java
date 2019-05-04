3e2c62790473271088052b089e8d09eedc92b542
ILYA <korennoi.ilya>
2019-05-04 17:54:39 +0300

    add lexer tests

---
+++ tests/com/github/redfoos/logstash/lexer/LogstashLexerTest.java
@@ -0,0 +1,27 @@
+package com.github.redfoos.logstash.lexer;
+
+import com.github.redfoos.logstash.LogstashLexerAdapter;
+import com.intellij.lexer.Lexer;
+import com.intellij.testFramework.LexerTestCase;
+
+public class LogstashLexerTest extends LexerTestCase {
+    @Override
+    protected Lexer createLexer() {
+        return new LogstashLexerAdapter();
+    }
+
+
+    public void testRegexp() {
+        doTest("/^someRegExp$/", "Logstash token type.REGEXP ('/^someRegExp$/')");
+    }
+
+    public void testString() {
+        doTest("\"double quoted string\"", "Logstash token type.STRING ('\"double quoted string\"')");
+        doTest("'single quoted string'", "Logstash token type.STRING (''single quoted string'')");
+    }
+
+    @Override
+    protected String getDirPath() {
+        return "";
+    }
+}
