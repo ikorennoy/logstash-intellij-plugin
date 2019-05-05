625705d9d931a26118de7f7926970bc0e2746f8c
redfoos <korennoi.ilya@yandex.ru>
2019-05-05 23:28:31 +0300

    move

---
+++ src/com/github/redfoos/logstash/formatter/LogstashBlock.java
@@ -0,0 +1,58 @@
+//package com.github.redfoos.logstash.formatter;
+//
+//import com.intellij.formatting.*;
+//import com.intellij.lang.ASTNode;
+//import com.intellij.psi.TokenType;
+//import com.intellij.psi.formatter.common.AbstractBlock;
+//import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.Nullable;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class LogstashBlock extends AbstractBlock {
+//    private SpacingBuilder spacingBuilder;
+//
+//    protected LogstashBlock(@NotNull ASTNode node, @Nullable Wrap wrap, @Nullable Alignment alignment,
+//                            SpacingBuilder spacingBuilder) {
+//        super(node, wrap, alignment);
+//        this.spacingBuilder = spacingBuilder;
+//    }
+//
+//    @NotNull
+//    @Override
+//    public ChildAttributes getChildAttributes(int newChildIndex) {
+//        return new ChildAttributes(Indent.getSpaceIndent(4), Alignment.createAlignment());
+//    }
+//
+//    @Override
+//    protected List<Block> buildChildren() {
+//        List<Block> blocks = new ArrayList<>();
+//        ASTNode child = myNode.getFirstChildNode();
+//        while (child != null) {
+//            if (child.getElementType() != TokenType.WHITE_SPACE) {
+//                Block block = new LogstashBlock(child, Wrap.createWrap(WrapType.NONE, false), Alignment.createAlignment(),
+//                        spacingBuilder);
+//                blocks.add(block);
+//            }
+//            child = child.getTreeNext();
+//        }
+//        return blocks;
+//    }
+//
+//    @Override
+//    public Indent getIndent() {
+//        return Indent.getNoneIndent();
+//    }
+//
+//    @Nullable
+//    @Override
+//    public Spacing getSpacing(@Nullable Block child1, @NotNull Block child2) {
+//        return spacingBuilder.getSpacing(this, child1, child2);
+//    }
+//
+//    @Override
+//    public boolean isLeaf() {
+//        return myNode.getFirstChildNode() == null;
+//    }
+//}
