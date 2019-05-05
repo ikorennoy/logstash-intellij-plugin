4c946bd14f8282544bd6e6af2a35a77f57406faa
redfoos <korennoi.ilya@yandex.ru>
2019-05-05 21:55:26 +0300

    add tests

--- tests/com/github/redfoos/logstash/highlighting/HighlightingTest.java
+++ tests/com/github/redfoos/logstash/highlighting/HighlightingTest.java
@@ -1,38 +1,38 @@
-//package com.github.redfoos.logstash.highlighting;
-//
-//import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.nio.file.Files;
-//
-//public class HighlightingTest extends LightPlatformCodeInsightFixtureTestCase {
-//
-//    @Override
-//    protected String getTestDataPath() {
-//        return "testData/highlighting";
-//    }
-//
-//    public void testFunctionalCall() {
-//        doTest();
-//    }
-//
-//    public void testFunctionalDefinition() {
-//        doTest();
-//    }
-//
-//    private void doTest() {
-//        File file = new File(getTestDataPath(), getTestName(true) + ".conf");
-//
-//        String content = "";
-//        try {
-//            content = new String(Files.readAllBytes(file.toPath()));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        myFixture.configureByText(file.getName(), content);
-//        myFixture.testHighlighting();
-//
-//    }
-//}
+package com.github.redfoos.logstash.highlighting;
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class HighlightingTest extends LightPlatformCodeInsightFixtureTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "testData/highlighting";
+    }
+
+    public void testPluginBlockDefinition() {
+        doTest();
+    }
+
+    public void testFunctionalElement() {
+        doTest();
+    }
+
+    private void doTest() {
+        File file = new File(getTestDataPath(), getTestName(true) + ".conf");
+
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        myFixture.configureByText(file.getName(), content);
+        myFixture.testHighlighting();
+
+    }
+}
