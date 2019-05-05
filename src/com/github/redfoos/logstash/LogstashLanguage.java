71d9beef71bb232703e5e062d5c378a09f670779
ILYA <korennoi.ilya>
2019-05-05 14:58:24 +0300

    release 0.1

--- src/com/github/redfoos/logstash/LogstashLanguage.java
+++ src/com/github/redfoos/logstash/LogstashLanguage.java
@@ -1,10 +1,6 @@
 package com.github.redfoos.logstash;
 
 import com.intellij.lang.Language;
-import com.intellij.openapi.fileTypes.SingleLazyInstanceSyntaxHighlighterFactory;
-import com.intellij.openapi.fileTypes.SyntaxHighlighter;
-import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
-import org.jetbrains.annotations.NotNull;
 
 public class LogstashLanguage extends Language {
     public static final LogstashLanguage INSTANCE = new LogstashLanguage();
